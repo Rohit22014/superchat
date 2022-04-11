a11321fc4ce7ad1e14c09899b7498e07d5fffd41
Rohit Dina Nath <rohitdinanath@gmail.com>
2022-04-11 23:35:10 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, {useState} from "react";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth';
+
+import {useAuthState, useSignInWithGoogle} from "react-firebase-hooks/auth";
+import { useCollectionData} from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+   
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+    const [user] = useAuthState(auth);
+
+    return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+        <section>
+            {user ? <ChatRoom/> : <SignIn/>}
+        </section>
+    </div>
+  );
+}
+
+function SignIn() {
+    return(
+        <button onClick={useSignInWithGoogle}>Sign in with Google</button>
+    )
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+function ChatRoom() {
+
+    const messageRef = firestore.collection('messages');
+    const query = messageRef.orderBy('createdAt').limit(25);
+
+    const [messages] = useCollectionData(query, {idField:'id'});
+
+    const [formValue, setFormValue] = useState('');
+
+    const sendMessage = async(e) => {
+        e.preventDefault();
+
+        const {uid,photoURL} = auth.currentUser;
+
+        await messageRef.add({
+           text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        });
+
+        setFormValue('');
+    }
+
+    return (
+        <>
+            <div>
+                {messages && messages.map(msg => <ChatMessage key={msg.id}message={msg}/>)}
+            </div>
+
+            <form onSubmit={sendMessage}>
+
+                <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+                <button type="submit">submit</button>
+
+            </form>
+        </>
+    )
+}
+
+function ChatMessage(props) {
+    const {text,uid, photoURL} = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+
+    return (
+        <div className={'message ${messageClass}'}>
+            <img src={photoURL}/>
+            <p>{text}</p>
+        </div>
+    )
+
+}
+
+export default App;
